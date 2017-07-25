db0f9d8c9b099b1e872f07ddb5bc30d07063d911
Ruhee D'cunha <rdcunha2@vt.edu>
2017-07-25 15:06:22 -0400

    added tests for existing functions

--- tests/test_math.py
+++ tests/test_math.py
@@ -12,3 +12,11 @@ def test_add():
 def test_mult():
     assert cg.mult(3, 4) == 12
     assert cg.mult(4, 5) == 20
+
+def test_div():
+    assert cg.div(20, 4) == 5
+    assert cg.div(5, 2) == 2.5
+
+def test_mod():
+    assert cg.mod(4, 3) == 1
+    assert cg.mod(23, 7) == 2
